cd61bb7c1415506bbb8b58da10d8f80e0a596ca7
paolovalerdi <9915479+paolovalerdi@users.noreply.github.com>
2025-10-22 23:39:33 -0600

    disable debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_chat/router/screens/chat/chat_screen.dart';
 import 'package:movie_chat/service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   await setupServiceLocator();
   runApp(const MovieChatApp());
 }
@@ -15,6 +17,7 @@ class MovieChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoApp(
       title: 'Movies',
+      debugShowCheckedModeBanner: false,
       theme: CupertinoThemeData(
         brightness: Brightness.dark,
         primaryColor: Color(0xFF1D9963),
